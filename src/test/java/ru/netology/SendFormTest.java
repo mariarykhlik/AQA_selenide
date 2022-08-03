c3977436b8530ba8c2dbdc19fc0e82ae86e76049
mariarykhlik <maria.rykhlik@gmail.com>
2022-08-03 10:37:04 +0300

    Amend SendFormTest

--- src/test/java/ru/netology/SendFormTest.java
+++ src/test/java/ru/netology/SendFormTest.java
@@ -2,15 +2,22 @@ package ru.netology;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SendFormTest {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @BeforeEach
     void openWebService() {
         open("http://localhost:9999");
@@ -18,14 +25,16 @@ public class SendFormTest {
 
     @Test
     void shouldSendForm() {
+        String planningDate = generateDate(5);
         $("[data-test-id=city] input").val("Санкт-Петербург");
-        Date date = new Date();
-        String testDate = date.toString();
-        $("[data-test-id=date] input").val(testDate);
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").val(planningDate);
         $("[data-test-id=name] input").val("Петров-Водкин Иван");
         $("[data-test-id=phone] input").val("+79876543210");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + planningDate))
+                .shouldBe(visible, Duration.ofSeconds(15));
     }
 }
